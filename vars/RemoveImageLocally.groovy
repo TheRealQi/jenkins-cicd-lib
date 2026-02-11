6e65b39000d09c0dd03f8ea1334bfa690cbb4585
TheRealQi <therealqi@hotmail.com>
2026-02-11 14:59:54 +0200

    Fixed RemoveImageLocally script arguements issue

--- vars/RemoveImageLocally.groovy
+++ vars/RemoveImageLocally.groovy
@@ -1,8 +1,6 @@
-def call(String workdir, String imageRepo, String imageName, String imageTag) {
-    dir(workdir) {
+def call(String imageRepo, String imageName, String imageTag) {
         echo "Removing Docker image ${IMAGE_REPO}/${IMAGE_NAME}:${IMAGE_TAG} locally"
         sh "docker rmi ${IMAGE_REPO}/${IMAGE_NAME}:${IMAGE_TAG} || true"
         echo "Docker image ${IMAGE_REPO}/${IMAGE_NAME}:${IMAGE_TAG} removed locally"
-    }
 }
 
